5d1bcc03c833411ab4d2a6d5a4df500842124d6f
phamcongphuc <phamcongphuc32@gmail.com>
2023-03-20 10:41:42 +0700

    xóa file tạm khi upload

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { createReadStream, createWriteStream } from 'fs';
+import { createReadStream, createWriteStream, unlinkSync } from 'fs';
 import { extname, join } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -9,16 +9,27 @@ export class UploadService {
     const fileExt = extname(file.originalname);
     const fileName = `${uuidv4()}${fileExt}`;
     const filePath = join(__dirname, '../..', 'public', fileName);
-
+    // remove file 
+    const fileRm = join(__dirname, '../..',  file.path);
     return new Promise((resolve, reject) => {
       const stream = createReadStream(file.path);
       const writeStream = createWriteStream(filePath);
 
       stream.pipe(writeStream);
       writeStream.on('error', (err) => reject(err));
-      writeStream.on('finish', () =>
-        resolve(`${process.env.APP_URL}/public/${fileName}`),
-      );
+
+      writeStream.on('finish', async () => {
+        resolve(`${process.env.APP_URL}/public/${fileName}`);
+        try {
+          unlinkSync(fileRm);
+        } catch (error) {
+          console.log("xóa file thất bại");
+        }
+      
+      });
+     
     });
+   
   }
 }
+
