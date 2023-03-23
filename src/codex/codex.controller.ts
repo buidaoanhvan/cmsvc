fc8493151fcc41584f29a6beea5ed3b8bea69367
phamcongphuc <phamcongphuc32@gmail.com>
2023-03-23 08:07:03 +0700

    import

--- src/codex/codex.controller.ts
+++ src/codex/codex.controller.ts
@@ -20,7 +20,7 @@ import * as XLSX from 'xlsx';
 
 @Controller('codex')
 export class CodexController {
-  constructor(private readonly codexService: CodexService) {}
+  constructor(private readonly codexService: CodexService) { }
 
   @Post()
   @HasPermissions('P_REGISTER')
@@ -104,20 +104,65 @@ export class CodexController {
   }
 
   // TEST IMPORT 2
+  // @Post('import')
+  // @UseInterceptors(FileInterceptor('file'))
+  // async import(@UploadedFile() file: Express.Multer.File, @Body() body: { voucherId: number }) {
+  //   const { voucherId } = body;
+
+  //   try {
+  //     await this.codexService.import(file, +voucherId);
+  //     return {
+  //       statusCode: 200,
+  //       message: 'codex impoprt thành công!',
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       statusCode: 400,
+  //       message: console.log(error),
+  //     };
+  //   }
+  // }
   @Post('import')
   @UseInterceptors(FileInterceptor('file'))
-  async import(@UploadedFile() file: Express.Multer.File) {
-    try {
-      await this.codexService.import(file);
+  async importCsv(@UploadedFile() file: Express.Multer.File) {
+    //console.log(file.buffer.toString());
+    return this.codexService.importCsv(file.buffer.toString());
+  }
+
+  @Post('aaa')
+  async count() {
+    const result = await this.codexService.countOfCode();
+    if (result.success) {
       return {
-        statusCode: 200,
-        message: 'codex impoprt thành công!',
+        statusCode: result.code,
+        message: result.message,
+        data: result.data,
       };
-    } catch (error) {
+    } else {
       return {
-        statusCode: 400,
-        message: error,
+        statusCode: result.code,
+        message: result.message,
       };
     }
   }
+
+  @Post('download')
+  async download(@Body() body: { codeInt: number, voucher: number }) {
+    const { codeInt, voucher } = body;
+
+    const result = await this.codexService.download(codeInt, voucher);
+    if (result.success) {
+      return {
+        statusCode: result.code,
+        message: result.message,
+        data: result.data,
+      };
+    } else {
+      return {
+        statusCode: result.code,
+        message: result.message,
+      };
+    }
+  }
+
 }
--- src/codex/codex.service.ts
+++ src/codex/codex.service.ts
@@ -1,19 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCodexDto } from './dto/create-codex.dto';
 import { UpdateCodexDto } from './dto/update-codex.dto';
 import * as XLSX from 'xlsx';
+import { parse } from 'fast-csv';
+import { createReadStream } from 'fs';
 
 interface ExcelRow {
   codex: string;
-  is_used: number;
-  phone: string;
-  status: number;
 }
 @Injectable()
 export class CodexService {
   // [x: string]: any;
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async create(createCodexDto: CreateCodexDto) {
     const { voucherId, segmentId, codex, is_used, phone, status } =
@@ -117,29 +116,104 @@ export class CodexService {
     }
   }
 
-  async import(file: Express.Multer.File) {
-    const workbook = XLSX.read(file.buffer, { type: 'buffer' });
-    const batchSize = 1000;
+  // async import(file: Express.Multer.File, voucherId: number) {
+  //   const workbook = XLSX.read(file.buffer, { type: 'buffer' });
+  //   const batchSize = 200000;
 
-    const sheet = workbook.Sheets[workbook.SheetNames[0]];
-    const rows: ExcelRow[] = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+  //   const sheet = workbook.Sheets[workbook.SheetNames[0]];
+  //   const rows: ExcelRow[] = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-    for (let i = 1; i < rows.length; i += batchSize) {
-      const batch = rows.slice(i, i + batchSize);
-      const data = batch.map((row) => {
-        // console.log(row[0], row.status, row.phone); // Log the values of codex, status, and phone to check if they are null
+  //   for (let i = 1; i < rows.length; i += batchSize) {
+  //     const batch = rows.slice(i, i + batchSize);
+  //     const data = batch.map((row) => {
 
-        return {
-          codex: row[0],
-          is_used: row[1],
-          status: row[2],
-        };
-      });
+  //       return {
+  //         codex: row[0],
+  //         status: 1,
+  //         voucher_id: voucherId
+  //       };
+  //     });
 
-      await this.prisma.codex.createMany({
-        data,
-        skipDuplicates: true,
-      });
+  //     await this.prisma.codex.createMany({
+  //       data,
+  //       skipDuplicates: true,
+  //     });
+  //   }
+  // }
+
+  async countOfCode() {
+    console.log("count")
+    const result = await this.prisma.codex.aggregate({
+      where: { is_used: 0 },
+      _count: true
+    });
+    if (result) {
+      return {
+        success: true,
+        code: 200,
+        message: 'thành công!',
+        data: result,
+      };
+    } else {
+      return {
+        success: false,
+        code: 400,
+        message: ' không thành công!',
+      };
     }
   }
+
+  async download(codeInt: number, voucher: number) {
+
+    const result = await this.prisma.codex.findMany({
+      where: {
+        is_used: 0,
+        voucher_id: voucher
+      },
+      take: codeInt,
+      select: { codex: true, id: true }
+    });
+
+
+    // If there are not enough unused codexes with that voucher ID, return an error message
+    if (!result || result.length < codeInt) {
+      return {
+        success: true,
+        code: 200,
+        message: 'không đủ code!',
+      }
+    }
+    console.log(result);
+    // Mark the codexes as used
+    const codexIds = result.map((codex) => codex.id);
+    await this.prisma.codex.updateMany({
+      where: { id: { in: codexIds } },
+      data: { is_used: 1 }
+    });
+
+    // Return a success message and the downloaded codexes
+    return {
+      success: true,
+      code: 200,
+      message: 'xuất code thành công!',
+      data: result,
+    }
+  }
+
+
+  async importCsv(data: string) {
+    const rows = data.split('\n').slice(1);
+    const codexData = rows.map((row) => {
+      const [ codex] = row.split(',');
+      return {  codex };
+    });
+
+    const createdCodexes = await this.prisma.codex.createMany({
+      data: codexData,
+    });
+
+    return { createdCodexes };
+  }
 }
+
+
